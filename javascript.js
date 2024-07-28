d637322553d3af4588f158c94137ce3edaa3eeea
jimboscatfood <149596389+jimboscatfood@users.noreply.github.com>
2024-07-28 21:27:54 +0800

    Modify output of button event handler
    The logic of how the click event is handled is updated to ensure the
    user input an integer between 1 and 100.
    
    Once the user enters a valid input, a for loop is used to select all the
    square div elements and delete them one by one.
    
    The createGrid function is called after resetting the container.

--- javascript.js
+++ javascript.js
@@ -7,35 +7,47 @@ let gridNum = 16;
 const container_width = container.clientWidth;
 const container_height = container.clientHeight;
 
+document.addEventListener("DOMContentLoaded", () =>
+    {createGrid(gridNum)
+    //Add eventListener to change color
+    }
+)
 
-for (let i = 0; i < gridNum**2; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.style.width = (container_width/gridNum) + "px";
-    square.style.height = (container_height/gridNum) + "px";
-    container.appendChild(square);
-
-    console.log(container.style.width);
+function createGrid(gridNum) {
+    for (let i = 0; i < gridNum**2; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = (container_width/gridNum) + "px";
+        square.style.height = (container_height/gridNum) + "px";
+        container.appendChild(square);
+    }
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+        })
+    });
 }
 
-//Add eventListener to change color
-const squares = document.querySelectorAll(".square");
-squares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "black";
-    })
-});
 
 //Add button to top of screen to ask for number of squares
 const button = document.createElement("button");
 button.textContent = "Choose your grid size (from 1 to 100)";
 document.body.insertBefore(button, container);
 button.addEventListener("click", () => {
-        gridNum = prompt("Enter grid size (1-100)");
-        while (gridNum > 100 || gridNum < 1 || !Number.isInteger(gridNum)) {
-            if (gridNum == null) break;
-            else {gridNum = prompt("Enter grid size (1-100)");}
+        let userInput = prompt("Enter grid size (1-100)");
+        while (parseInt(userInput) > 100 || parseInt(userInput) < 1 || !Number.isInteger(parseInt(userInput))) {
+            if (userInput == null) break;
+            else {
+                userInput = prompt("Enter grid size (1-100)");
+            }
         }
-    }
-)
-//Then reset the grid with the new grid size
+        gridNum = parseInt(userInput);
+        //Then reset the grid with the new grid size
+        const squares = document.querySelectorAll(".square");
+        squares.forEach((square) => {
+            square.remove()
+        });
+        createGrid(gridNum);        
+    })
+
